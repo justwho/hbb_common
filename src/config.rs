5351bddbe8c5435d5ce312a881262e59880fdf95
justwho <rov7@qq.com>
2025-05-16 18:16:53 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["oa123.hicp.net"];
+pub const RS_PUB_KEY: &str = "PduLGPQAV6mnuc1cFRsJzvLxkZ0sMqsm5zmyanhzQXM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
